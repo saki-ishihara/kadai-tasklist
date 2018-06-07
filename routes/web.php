3f7ffc2a1d245bfda6e03c6eaba058165ccde27c
saki.ishihara <ec2-user@ip-172-31-36-72.us-east-2.compute.internal>
2018-06-07 06:43:57 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', 'TasklistsController@index');
 
 Route::resource('tasklists', 'TasklistsController');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
